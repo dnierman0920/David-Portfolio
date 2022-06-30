76da3a40a75fb75746486d7298c9a9ff900e787b
dnierman0920 <davidnierman93@gmail.com>
2022-06-30 09:54:37 -0700

    fix mistake change 'server' to 'app'

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-var port_number = server.listen(process.env.PORT || 3000);
+var port_number = app.listen(process.env.PORT || 3000);
 app.listen(port_number)
 
 // Integrating EJS for templating
